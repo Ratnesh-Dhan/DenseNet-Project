f26e8e3948dc37d4a08c33e21ca29de683cbb366
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-01-28 10:57:46 +0530

    training densenet code

--- src/TraningDenseNet.py
+++ src/TraningDenseNet.py
@@ -6,3 +6,109 @@ Created on Tue Jan 28 10:27:20 2025
 @author: Paddy King
 """
 
+import TensorFlow as tf
+
+# We will use the CIFAR-10 dataset, which consists of 60,000 32x32 color images in 10 classes.
+# There are 50,000 training images and 10,000 test images. 
+# The dataset is available in the TensorFlow Keras API, so we can load it directly.
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+
+#preprocess the data by normalizing the pixel values and one-hot encoding the labels.
+train_images = train_images.astype('float32') / 255
+test_images = test_images.astype('float32') / 255
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+#BUILDING THE MODEL
+class DenseLayer(tf.keras.layers.Layer):
+    def __init__(self, growth_rate):
+        super(DenseLayer, self).__init__()
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.relu1 = tf.keras.layers.Activation('relu')
+        self.conv1 = tf.keras.layers.Conv2D(filters=4 * growth_rate, kernel_size=(1, 1), padding='same', use_bias=False)
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.relu2 = tf.keras.layers.Activation('relu')
+        self.conv2 = tf.keras.layers.Conv2D(filters=growth_rate, kernel_size=(3, 3), padding='same', use_bias=False)
+    
+    def call(self, inputs):
+        x = self.bn1(inputs)
+        x = self.relu1(x)
+        x = self.conv1(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.conv2(x)
+        output = tf.keras.layers.concatenate([inputs, x], axis=-1)
+        return output
+
+class DenseBlock(tf.keras.layers.Layer):
+    def __init__(self, num_layers, growth_rate):
+        super(DenseBlock, self).__init__()
+        self.layers = [DenseLayer(growth_rate) for _ in range(num_layers)]
+        
+    def call(self, inputs):
+        x = inputs
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+class TransitionLayer(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        super(TransitionLayer, self).__init__()
+        self.bn = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.Activation('relu')
+        self.conv = tf.keras.layers.Conv2D(filters=filters, kernel_size=(1, 1), padding='same', use_bias=False)
+        self.pool = tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')
+    
+    def call(self, inputs):
+        x = self.bn(inputs)
+        x = self.relu(x)
+        x = self.conv(x)
+        output = self.pool(x)
+        return output
+
+class DenseNet(tf.keras.Model):
+    def __init__(self, num_blocks=3, num_layers=16, growth_rate=12, num_classes=10):
+        super(DenseNet, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(filters=2 * growth_rate, kernel_size=(3, 3), padding='same', use_bias=False)
+        self.blocks = [DenseBlock(num_layers, growth_rate) for _ in range(num_blocks)]
+        self.trans_layers = [TransitionLayer(num_layers * growth_rate) for _ in range(num_blocks - 1)]
+        self.bn = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.Activation('relu')
+        self.pool = tf.keras.layers.GlobalAveragePooling2D()
+        self.fc = tf.keras.layers.Dense(units=num_classes, activation='softmax')
+    
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        for i in range(len(self.blocks)):
+            x = self.blocks[i](x)
+            if i != len(self.blocks) - 1:
+                x = self.trans_layers[i](x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        output = self.fc(x)
+        return output
+    
+    
+# TRAINING MODEL
+model = DenseNet(num_blocks=3, num_layers=16, growth_rate=12, num_classes=10)
+
+model.compile(
+  optimizer=tf.keras.optimizers.Adam(), 
+  loss=tf.keras.losses.categorical_crossentropy, 
+  metrics=['accuracy']
+)
+
+model.fit(
+  train_images, 
+  train_labels, 
+  epochs=100, 
+  batch_size=64, 
+  validation_data=(test_images, test_labels)
+)
+
+# Evaluation
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Test loss : ", test_loss)
+print("Test accuracy : ", test_acc)
