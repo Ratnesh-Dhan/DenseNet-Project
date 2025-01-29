8e202db8198c3d702a761f9e9b63036f02c56535
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-01-29 11:57:25 +0530

    stegmentation starting with deeplabv3

--- src/Segmentation/deeplabv3Segmentation.py
+++ src/Segmentation/deeplabv3Segmentation.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
+# USING EXISTING MODEL FROM KAGGLE NAMED deeplabv3 FOR SEGMENTAION
 
+import cv2
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+
+model_path = "../../TrainedModel/2.tflite"
+
+model = tf.keras.models.
