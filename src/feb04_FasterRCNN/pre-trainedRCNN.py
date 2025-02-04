ee270c83129f7ccabffa3d482bab9cc25889a66b
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-02-04 14:57:49 +0530

    working pre-trainedRCNN

--- src/feb04_FasterRCNN/pre-trainedRCNN.py
+++ src/feb04_FasterRCNN/pre-trainedRCNN.py
@@ -11,7 +11,7 @@ above is the the link where you can find the models . choose models according to
 
 import tensorflow as tf
 import cv2
-import sys
+
 
 image_path = "../img/sample-street1.jpg"
 model_dir = '../../pre-Trained_model/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8/saved_model'
@@ -67,26 +67,3 @@ for box, score, cls in zip(boxes[0], scores[0], classes[0]):
 
 cv2.imshow("Prediction", display_image)
 cv2.waitKey(0)
-
-#THIS IS ORIGINAL CODE AND ABOVE IS IMPROVEMENT TESTING
-sys.exit(0)
-#Visualize results
-# image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-for box, score, cls in zip(boxes[0], scores[0], classes[0]):
-    if score > 0.5:
-        print(box , score)
-        y1, x1, y2, x2 = box
-        print("this is y1 and x1 coordinates of the box ",y1," ", x1)
-        # x1, y1, x2, y2 = box
-        y1, x1, y2, x2 = int(y1 * image.shape[1]), int(x1 * image.shape[2]), int(y2 * image.shape[1]), int(x2 * image.shape[2])
-        cv2.rectangle(image[0].numpy(), (x1, y1), (x2, y2), (0, 255, 0), 2)
-        # image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(image[0].numpy(), f"Class {int(cls)}", (x1, y1 - 10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (255,0,0), 2)
-        # cv2.putText(image, f"Class {int(cls)}", (x1, y1 - 10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (255,0,0), 2)
-    
-# plt.imshow(image[0].numpy())
-# plt.axis('off')
-# plt.show()
-cv2.imshow("Prediction", image[0].numpy())
-cv2.waitKey(0)
-        
