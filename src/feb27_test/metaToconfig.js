11229ff292356a598fa2bbce06e13d2723404187
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-02-28 11:09:39 +0530

    creating label_map.pbtxt and js script to make label_map.pbtxt

---
+++ src/feb27_test/metaToconfig.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+function readJsonFile(filePath) {
+  try {
+    const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+    // Writing label_map.pbtxt file
+    const labelMapContent = data.classes
+      .map((element) => {
+        return `item {\n id: ${element.id} \n name: '${element.title}' \n color: '${element.color}'\n}`;
+      })
+      .join("\n");
+
+    fs.writeFileSync("label_map.pbtxt", labelMapContent, "utf8");
+
+    // return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    return null;
+  }
+}
+
+// Example usage
+readJsonFile("../../Datasets/pcbDataset/meta.json");
