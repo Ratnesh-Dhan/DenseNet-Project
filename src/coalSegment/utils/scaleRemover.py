f8a24196ef1e2d3253f1d59eb82e70fe1930b3b0
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-04-17 17:53:06 +0530

    scale remover fixed , percentage color matched with the segmented image

--- src/coalSegment/utils/scaleRemover.py
+++ src/coalSegment/utils/scaleRemover.py
@@ -2,28 +2,32 @@ import os
 from matplotlib import pyplot as plt
 import cv2 
 
-# folder_path = r"D:\NML ML Works\Coal photomicrographs\Coal_Lebels"
-folder_path = r"D:\NML ML Works\Coal photomicrographs"
-
-# files = os.listdir(folder_path)
-files = [file for file in os.listdir(folder_path) if file.endswith('.jpg')]
-print(files)
-# import sys
-# sys.exit(0)
-
-for file in files:
-    image = cv2.imread(os.path.join(folder_path, file))
-    # Define rectangle coordinates (top-left and bottom-right corners)
-    top_left = (2144, 36)
-    bottom_right = (2572, 162)
-
+def scale_remover(image, top_left=(2123, 36), bottom_right=(2572, 162)):
+    """
+    Removes the scale from a coal photomicrograph image by drawing a black rectangle over it.
+    
+    Args:
+        image (numpy.ndarray): Input image
+        top_left (tuple): Top-left coordinates of the scale (default: (2123, 36))
+        bottom_right (tuple): Bottom-right coordinates of the scale (default: (2572, 162))
+    
+    Returns:
+        numpy.ndarray: Image with scale removed
+    """
     # Draw filled rectangle on the image
-    cv2.rectangle(image, top_left, bottom_right, (0, 0, 0), -1)  # Red color with filled rectangle
+    cv2.rectangle(image, top_left, bottom_right, (0, 0, 0), -1)
+    return image
 
-    cv2.imwrite(os.path.join(folder_path, 'scaleRemoved/'+file), image)
-# plt.imshow(image)
-# plt.axis("off")
-# plt.show()
+if __name__ == '__main__':
+    # Example usage
+    folder_path = r"D:\NML ML Works\Coal photomicrographs"
+    files = [file for file in os.listdir(folder_path) if file.endswith('.jpg')]
+
+    for file in files:
+        image = cv2.imread(os.path.join(folder_path, file))
+        image = scale_remover(image=image)
+        cv2.imwrite(os.path.join(folder_path, 'scaleRemoved/'+file), image)
+
 
 # 2141, 28
 # 2572, 161
