a494834aab3fa99caa89ea7ef67fdbdc6cffe3d1
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-04-23 16:17:01 +0530

    updates.

--- src/april22_corrosion/utils/classIndexTo0.js
+++ src/april22_corrosion/utils/classIndexTo0.js
@@ -2,13 +2,14 @@ const fs = require("fs");
 const path = require("path");
 
 const base_location =
-  "C:\\Users\\NDT Lab\\Pictures\\dataset\\archive\\corrosion detect";
-const labels = path.join(base_location, "labels");
+  "C:\\Users\\NDT Lab\\Pictures\\dataset\\archive\\corrosion detect\\validation";
+const labels = path.join(base_location, "ann");
 const files = fs.readdirSync(labels);
 
 let file_path = base_location;
+let count = 1;
 files.forEach((element) => {
-  file_path = path.join(base_location, "labels", element);
+  file_path = path.join(base_location, "ann", element);
   const file = fs.readFileSync(file_path, "utf8");
 
   const lines = file.split("\n");
@@ -19,4 +20,5 @@ files.forEach((element) => {
     return parts.join(" ");
   });
   fs.writeFileSync(file_path, newLines.join("\n"));
+  console.log(`${count++} / ${files.length}`);
 });
