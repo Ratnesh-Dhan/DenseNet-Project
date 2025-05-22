7c02b562b66e2d75a5127e22068a747c6e19b4c7
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-05-22 18:29:25 +0530

    early stopping added and model will be saved as .keras format

--- src/may20_unet_resnetBackbone/train/train.py
+++ src/may20_unet_resnetBackbone/train/train.py
@@ -1,7 +1,7 @@
 # train_unet.py
 from model import build_unet
 from dataset_loader import CorrosionDataset
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import tensorflow as tf
 import os
 
@@ -27,11 +27,23 @@ base_location = r'D:\NML ML Works\kaggle_semantic_segmentation_CORROSION_dataset
 train_gen = CorrosionDataset(os.path.join(base_location, "train/images"), os.path.join(base_location, "train/masks"))
 val_gen = CorrosionDataset(os.path.join(base_location, "validate/images"), os.path.join(base_location, "validate/masks"))
 
+checkpoint_path = "../models/checkpoints/unet_best_checkpoint.keras"
+os.makedirs(os.path.dirname(checkpoint_path), exist_ok=True)
+
+checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_path,
+    monitor='val_loss',
+    save_best_only=True,
+    save_weights_only=False,  # Set to True if you want to save only weights
+    mode='min',
+    verbose=1
+)
+
 model = build_unet(input_shape=(256, 256, 3))
 # model.compile(optimizer="adam", loss=bce_dice_loss, metrics=["accuracy"])
 model.compile(optimizer='adam',
               loss=bce_dice_loss,
               metrics=[dice_loss])
 
-model.fit(train_gen, validation_data=val_gen, epochs=20, callbacks=[early_stopping])
-model.save("../models/unet_resnet50_corrosion_office_version.h5")
+model.fit(train_gen, validation_data=val_gen, epochs=20, callbacks=[early_stopping, checkpoint_callback])
+model.save("../models/unet_resnet50_corrosion_office_version.keras")
