5b0ff9d0b5cc77da95f2313a5101f79aa43d2f00
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-06-06 17:48:08 +0530

    improvements on prediction script

--- src/June02_coalSegments/train/prediction.py
+++ src/June02_coalSegments/train/prediction.py
@@ -4,15 +4,17 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-def sliding_window_inference(model, image, window_size=31, stride=10, center_patch_size=5):
+def sliding_window_inference(model, image, window_size=31, stride=15, center_patch_size=15):
     h, w, _f = image.shape
+    print(h,w)
     heatmap = np.zeros((h, w, 3), dtype=np.uint8)
 
     half_patch = window_size // 2
     half_center = center_patch_size // 2
 
-    for y in range(0, h - window_size + 1, stride):
-        for x in range(0, w - window_size + 1, stride):
+    for y in range(15, h - window_size + 1, stride):
+        print(y)
+        for x in range(15, w - window_size + 1, stride):
             patch = image[y:y+window_size, x:x+window_size].astype(np.float32) / 255.0
             # patch = np.expand_dims(patch, axis=(0, -1))  # shape: (1, 31, 31, 1)
             patch = np.expand_dims(patch, axis=0)  # for RGB 
@@ -23,7 +25,7 @@ def sliding_window_inference(model, image, window_size=31, stride=10, center_pat
             center_y = y + half_patch
             center_x = x + half_patch
             # color = (0, 255, 0) if pred_class == 0 else (0, 0, 255)  # Organic or Inorganic
-            print(f'Class found: {pred_class}')
+            # print(f'Class found: {pred_class}')
             if pred_class == 0:
                 color = (0, 255, 0)
             elif pred_class == 1:
@@ -43,11 +45,12 @@ def sliding_window_inference(model, image, window_size=31, stride=10, center_pat
     return heatmap
 
 model = tf.keras.models.load_model("model.keras")
-file = "../img/010.jpg"
+file_name = "010"
+file = f"../img/{file_name}.jpg"
 img = plt.imread(f"../img/{file}")
 heatmap = sliding_window_inference(model, img)
 
-cv2.imwrite(f"./results/{img}_heatmap.png", heatmap)
+cv2.imwrite(f"./results/{file_name}_heatmap.png", heatmap)
 plt.subplot(1, 2, 1)
 plt.imshow(img)
 plt.title("Input Image")
@@ -57,5 +60,5 @@ plt.imshow(heatmap)
 plt.title("Segmentation Result")
 plt.axis('off')
 plt.tight_layout()
-plt.savefig(f"./results/{img}_comparison.png")
+plt.savefig(f"./results/{file_name}_comparison.png")
 plt.show()
