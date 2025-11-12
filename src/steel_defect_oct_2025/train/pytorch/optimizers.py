training with pytorch after fixing precision , recall & F1 methods

src/steel_defect_oct_2025/train/pytorch/optimizers.py
@@ -18,13 +18,13 @@ from earlystopping import EarlyStopping
 from xmldataset import XMLDataset
 
 # ====================== CONFIG ======================
-# ROOT_DIR = "../../../../Datasets/NEU-DET/"
-ROOT_DIR = "/mnt/d/Codes/DenseNet-Project/Datasets/NEU-DET/"
+ROOT_DIR = "../../../../Datasets/NEU-DET/"
+# ROOT_DIR = "/mnt/d/Codes/DenseNet-Project/Datasets/NEU-DET/"
 CLASSES_FILE = os.path.join(ROOT_DIR, "classes.txt")
 BATCH_SIZE = 8
-NUM_EPOCHS = 1
+NUM_EPOCHS = 25
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-RESULTS_DIR = "./results/optimizer_comparison_20251111"
+RESULTS_DIR = "./results/optimizer_comparison_20251112"
 SCORE_THRESHOLD = 0.3
 IOU_THRESHOLD = 0.5
 
@@ -42,26 +42,26 @@ OPTIMIZER_CONFIGS = {
         'params': {'lr': 0.0005, 'betas': (0.9, 0.999), 'weight_decay': 0.001}, # Often 1e-3 or 1e-4 works better in weight decay.
         'color': 'green'
     },
-    # 'SGD': {
-    #     'optimizer': optim.SGD,
-    #     'params': {'lr': 0.01, 'momentum': 0.9, 'weight_decay': 0.0001}, # decent, but LR 0.001 is too low for plain SGD. Try 0.01 or 0.1.
-    #     'color': 'red'
-    # },
-    # 'SGD_Nesterov': {
-    #     'optimizer': optim.SGD,
-    #     'params': {'lr': 0.01, 'momentum': 0.9, 'weight_decay': 0.0001, 'nesterov': True},
-    #     'color': 'orange'
-    # },
-    # 'RMSprop': {
-    #     'optimizer': optim.RMSprop,
-    #     'params': {'lr': 1e-4, 'alpha': 0.9, 'weight_decay': 1e-5},
-    #     'color': 'purple'
-    # },
-    # 'Adagrad': {
-    #     'optimizer': optim.Adagrad,
-    #     'params': {'lr': 0.001, 'weight_decay': 0.0001},
-    #     'color': 'brown'
-    # }
+    'SGD': {
+        'optimizer': optim.SGD,
+        'params': {'lr': 0.01, 'momentum': 0.9, 'weight_decay': 0.0001}, # decent, but LR 0.001 is too low for plain SGD. Try 0.01 or 0.1.
+        'color': 'red'
+    },
+    'SGD_Nesterov': {
+        'optimizer': optim.SGD,
+        'params': {'lr': 0.01, 'momentum': 0.9, 'weight_decay': 0.0001, 'nesterov': True},
+        'color': 'orange'
+    },
+    'RMSprop': {
+        'optimizer': optim.RMSprop,
+        'params': {'lr': 1e-4, 'alpha': 0.9, 'weight_decay': 1e-5},
+        'color': 'purple'
+    },
+    'Adagrad': {
+        'optimizer': optim.Adagrad,
+        'params': {'lr': 0.001, 'weight_decay': 0.0001},
+        'color': 'brown'
+    }
 }
 
 # ====================== TRAINING ======================