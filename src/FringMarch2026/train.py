7111193b8948c1b7d879e524aa2932f50e6ba242
Ratnesh-Dhan <ratneshdhan@gmail.com>
2026-03-06 00:00:03 +0530

    fixing script but the model is garbage because of only limited number of dataset

--- src/FringMarch2026/train.py
+++ src/FringMarch2026/train.py
@@ -10,13 +10,15 @@ from sklearn.metrics import r2_score
 dataset = FringeDataset("/home/zumbie/Codes/NML/Datasets/bmp","/home/zumbie/Codes/NML/Datasets/heightmaps")
 train_losses = []
 val_losses = []
+train_rmse_list = []
+val_rmse_list = []
 train_size = int(0.8*len(dataset))
 val_size = len(dataset) - train_size
 
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=4, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
 # loader = DataLoader(dataset,batch_size=4,shuffle=True)
 
@@ -33,6 +35,7 @@ for epoch in range(100):
     #-------------TRAIN-------------
     model.train()
     train_loss = 0
+    train_rmse = 0
     for img, height in tqdm(train_loader):
 
         img = img.cuda()
@@ -47,13 +50,18 @@ for epoch in range(100):
         optimizer.step()
         
         train_loss += loss.item()
-        train_losses.append(train_loss)
+        train_rmse += torch.sqrt(torch.mean((pred-height)**2)).item()
+
+
     train_loss /= len(train_loader)
+    train_losses.append(train_loss)
+    train_rmse_list.append(train_rmse)
 
     #----------VALIDATION--------------
     print("Validation starts ............................")
     model.eval()
     val_loss = 0
+    val_rmse = 0
     with torch.no_grad():
         for img, height in val_loader:
             img = img.cuda()
@@ -62,9 +70,11 @@ for epoch in range(100):
             pred = model(img)
             loss = loss_fn(pred, height)
             val_loss += loss.item()
-            val_losses.append(val_loss)
-    
+            val_rmse += torch.sqrt(torch.mean((pred-height)**2)).item()
+
     val_loss /= len(val_loader)
+    val_losses.append(val_loss)
+    val_rmse_list.append(val_rmse)
     print(f"Epoch {epoch} | Train {train_loss:.4f} | Val {val_loss:.4f}")
 
     # ---------------CHECKPOINT------------------
@@ -91,9 +101,8 @@ plt.title("Training vs Validation Loss")
 plt.savefig("loss.png")
 plt.show()
 
-rmse = torch.sqrt(torch.mean((pred-height)**2))
-plt.plot(train_rmse,label="Train RMSE")
-plt.plot(val_rmse,label="Val RMSE")
+plt.plot(train_rmse_list,label="Train RMSE")
+plt.plot(val_rmse_list,label="Val RMSE")
 plt.legend()
 plt.title("RMSE per Epoch")
 plt.savefig("rmse.png")
