22d23d7d4af20425beff6ddaac188463975a90e2
Ratnesh-Dhan <ratneshdhan@gmail.com>
2026-03-11 18:12:07 +0530

    changes in output

--- src/FringMarch2026/inference.py
+++ src/FringMarch2026/inference.py
@@ -3,25 +3,33 @@ import torch
 from services.model import UNet
 import cv2
 from matplotlib import pyplot as plt
-
-img = cv2.imread("/mnt/d/DATASETS/mntFiles/bmp/1.bmp",0)
-# img = img/255.0
-img = img.astype(np.float32) / 255.0
-
-tensor = torch.tensor(img).unsqueeze(0).unsqueeze(0).float().cuda()
+from tqdm import tqdm
 
 model = UNet()
 model.load_state_dict(torch.load("best_model.pth"))
 model.eval()
 model.cuda()
-# pred = model(tensor)
-with torch.no_grad():
-    pred = model(tensor)
-# height = pred.squeeze().cpu().detach().numpy()
-height = pred.squeeze().cpu().numpy()
-plt.plot(height[512][:])
-plt.show()
-exit(0)
-np.save("pred_height.npy",height)
-# or
-np.savetxt("pred_height.csv",height,delimiter=",")
+
+for i in tqdm(range(10,31)):
+    img = cv2.imread(f"/mnt/d/DATASETS/mntFiles/bmp/{i}.bmp",0)
+    # img = img/255.0
+    img = img.astype(np.float32) / 255.0
+
+    tensor = torch.tensor(img).unsqueeze(0).unsqueeze(0).float().cuda()
+
+    # pred = model(tensor)
+    with torch.no_grad():
+        pred = model(tensor)
+    # height = pred.squeeze().cpu().detach().numpy()
+    height = pred.squeeze().cpu().numpy()
+    # plt.plot(height[512][:])
+    # plt.show()
+
+    plt.imshow(height, cmap='jet')
+    plt.axis("off")
+    plt.title(f"{i}.bmp")
+    plt.savefig(f"./saved/{i}.png")
+    # exit(0)
+    # np.save("pred_height.npy",height)
+    # or
+    np.savetxt(f"./saved/{i}_pred_height.csv",height,delimiter=",")
--- src/FringMarch2026/printing_vertical_line.py
+++ src/FringMarch2026/printing_vertical_line.py
@@ -3,41 +3,49 @@ import torch
 from services.model import UNet
 import cv2
 from matplotlib import pyplot as plt
-filepath = "/mnt/d/DATASETS/mntFiles/heightmaps/2_height_map.npy"
-img = cv2.imread("/mnt/d/DATASETS/mntFiles/bmp/1.bmp",0)
-# img = img/255.0
-img = img.astype(np.float32) / 255.0
-
-tensor = torch.tensor(img).unsqueeze(0).unsqueeze(0).float().cuda()
+from tqdm import tqdm
 
 model = UNet()
 model.load_state_dict(torch.load("best_model.pth"))
 model.eval()
 model.cuda()
-# pred = model(tensor)
-with torch.no_grad():
-    pred = model(tensor)
-# height = pred.squeeze().cpu().detach().numpy()
-height = pred.squeeze().cpu().numpy()
 
-heightmapO = np.load(filepath)
-mean = heightmapO.mean()
-std = heightmapO.std()
+for i in tqdm(range(31, 100)):    
+    filepath = f"/mnt/d/DATASETS/mntFiles/heightmaps/{i}_height_map.npy"
+    img = cv2.imread(f"/mnt/d/DATASETS/mntFiles/bmp/{i}.bmp",0)
+    # img = img/255.0
+    img = img.astype(np.float32) / 255.0
 
-heightmapO = (heightmapO - mean) / std
+    tensor = torch.tensor(img).unsqueeze(0).unsqueeze(0).float().cuda()
 
-vmin = min(heightmapO.min(), height.min())
-vmax = max(heightmapO.max(), height.max())
+    # pred = model(tensor)
+    with torch.no_grad():
+        pred = model(tensor)
+    # height = pred.squeeze().cpu().detach().numpy()
+    height = pred.squeeze().cpu().numpy()
 
-print(heightmapO[512][:])
-plt.subplot(2,2,1)
-plt.imshow(heightmapO, cmap='jet', vmin=vmin, vmax=vmax)
-plt.subplot(2,2,3)
-plt.plot(heightmapO[512][:])
+    heightmapO = np.load(filepath)
+    mean = heightmapO.mean()
+    std = heightmapO.std()
 
-plt.subplot(2,2,2)
-plt.imshow(height, cmap='jet', vmin=vmin, vmax=vmax)
-plt.subplot(2,2,4)
-plt.plot(height[512][:])
-plt.savefig("4image.png")
-plt.show()
+    heightmapO = (heightmapO - mean) / std
+
+    vmin = min(heightmapO.min(), height.min())
+    vmax = max(heightmapO.max(), height.max())
+
+    # print(heightmapO[512][:])
+    plt.subplot(2,2,1)
+    plt.imshow(heightmapO, cmap='jet', vmin=vmin, vmax=vmax)
+    plt.title("Height data of input image")
+    plt.subplot(2,2,3)
+    plt.plot(heightmapO[512][:])
+    plt.ylim(bottom=0)
+
+    plt.subplot(2,2,2)
+    plt.imshow(height, cmap='jet', vmin=vmin, vmax=vmax)
+    plt.title("Height data output")
+    plt.subplot(2,2,4)
+    plt.plot(height[512][:])
+    plt.ylim(bottom=0)
+    plt.savefig(f"./final_result/{i}_4_figure.png")
+    plt.close()
